d686fdcde4fafdbe9038c8569ac00569ab1eb283
Ashekhawat@2001 <ankush75056@gmail.com>
2023-08-30 17:36:11 +0530

    change

--- src/main/java/com/driver/OrderRepository.java
+++ src/main/java/com/driver/OrderRepository.java
@@ -1,11 +1,14 @@
 package com.driver;
 
 
+import org.springframework.stereotype.Repository;
+
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+@Repository
 public class OrderRepository {
 
     Map<String,Order> ordersDb = new HashMap<>();
